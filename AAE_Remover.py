1623242afea6392b8fe8af82401713769bdff10a
Seth Barrett <sebarrett@augusta.edu>
2021-03-04 15:41:52 -0500

    Made improvements

--- AAE_Remover.py
+++ AAE_Remover.py
@@ -1,9 +1,6 @@
 '''
 Created by Seth Barrett
 
-
-
-
 This project is designed to remove all files with the extension .AAE. 
 These are founnd whenever I back up my iPhone's photos and are annoying lol.
 
@@ -20,15 +17,27 @@ Opening Message
 print(f'\n{"":-^60}\n{"AAE File Remover":-^60}\n{"For iPhone Photo Backup Cleanup":-^60}\n{"Created by Seth Barrett":-^60}\n{"King":-^60}\n{"":-^60}')
 '''
 -----------------------------------------------------------------------
-Show Current Working Directory, Ask for Input and Open Input file, Delete files
+Show Current Working Directory, Ask for Input and Choose Directory
 -----------------------------------------------------------------------
 '''
-print(f'Current Working Directory:{os.getcwd()}')
-dirs = [ f for f in os.listdir( os.curdir ) if os.path.isdir(f) ]
-for dir in dirs:
-    print(f'{"-"*5}{dir}')
-#Add UIV here
-ufolder = str(input('''Please choose the directory wish that contains all the .AAE files:'''))
+boolUI = False
+ufolder = ''
+while not boolUI:
+    print(f'Current Working Directory:{os.getcwd()}')
+    dirs = [ f for f in os.listdir( os.curdir ) if os.path.isdir(f) ]
+    for dir in dirs:
+        print(f'{"-"*5}{dir}')
+    uifolder = str(input('Please choose the directory wish that contains all the .AAE files:'))
+    if uifolder in dirs:
+        ufolder = uifolder
+        boolUI = True
+        break
+    print(f'\n{"":-^60}Type a correct directory\'s name.\n{"":-^60}')
+'''
+-----------------------------------------------------------------------
+Delete all files ending in .aae, List all deleted Files
+-----------------------------------------------------------------------
+'''
 delfiles = []
 for root, dirpath, files in os.walk(ufolder):
     for file in files:
